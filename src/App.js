430733209981d3a6163cd3d9c104300480ba715d
arbansal <arpit.bansal@radisys.com>
2024-07-16 20:37:20 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect } from 'react';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { updateAirports } from './features/Search/Airports.slice';
+import { Search } from './features/Search';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const fetchAirports = useCallback(async () => {
+    const response = await fetch(`${process.env.PUBLIC_URL}/airports.json`);
+    const data = await response.json()
+    dispatch(updateAirports(data))
+  }, []);
+
+  useEffect(() => {
+    fetchAirports();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", flexDirection: "column" }}>
+      <Search />
     </div>
   );
 }
